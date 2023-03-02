2da91be02a216543d3c708cf1d3439e47e63ddc4
Aaron <pinatamostgrim@proton.me>
2023-03-02 15:07:40 -0400

    Simulate register-only MOV instruction

--- part01/perfaware_8086_decoder.cpp
+++ part01/perfaware_8086_decoder.cpp
@@ -1,34 +1,134 @@
-#include "Windows.h"
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define internal static
+#define global_variable static
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+
+// TODO (Aaron): Disable after testing
+#if 1
+#define Assert(Expression) if (!(Expression)) {*(int *)0 = 0;}
+#else
+#define Assert(Expression)
+#endif
+
+global_variable const char *RegisterEncodingsW0[]
+{
+    "al",
+    "cl",
+    "dl",
+    "bl",
+    "ah",
+    "ch",
+    "dh",
+    "bh",
+};
+
+global_variable const char *RegisterEncodingsW1[]
+{
+    "ax",
+    "cx",
+    "dx",
+    "bx",
+    "sp",
+    "bp",
+    "si",
+    "di",
+};
+
+internal void decode_instruction(uint8 byte1, uint8 byte2)
+{
+    const char *instructionStr;
+    const char *destStr;
+    const char *sourceStr;
+
+    uint8 operation = byte1 >> 2;
+    uint8 direction = (byte1 >> 1) & (0b00000001);
+    uint8 w = byte1 & 0b00000001;
+
+    uint8 mod = (byte2 >> 6);
+    uint8 reg = (byte2 >> 3) & (0b00000111);
+    uint8 rm = byte2 & 0b00000111;
+
+    if (operation == 34)
+    {
+        instructionStr = "mov";
+        if(mod == 3)
+        {
+            if (w == 1)
+            {
+                destStr = (direction == 1) ? RegisterEncodingsW1[reg] : RegisterEncodingsW1[rm];
+                sourceStr = (direction == 1) ? RegisterEncodingsW1[rm] : RegisterEncodingsW1[reg];
+            }
+            else
+            {
+                destStr = (direction == 1) ? RegisterEncodingsW0[reg] : RegisterEncodingsW0[rm];
+                sourceStr = (direction == 1) ? RegisterEncodingsW0[rm] : RegisterEncodingsW0[reg];
+            }
+        }
+        else
+        {
+            destStr = "?";
+            sourceStr = "?";
+        }
+    }
+    else
+    {
+        instructionStr = "?";
+        destStr = "?";
+        sourceStr = "?";
+    }
+
+    printf("%s %s,%s\n", instructionStr, destStr, sourceStr);
+}
 
 
 int main(int argc, char const *argv[])
 {
-    char AssemblyFilename[] = "listing_0037_single_register_mov";
-    printf("opening assembly '%s'\n", AssemblyFilename);
+    // TODO (Aaron): accept command line argument for file
 
-    HANDLE AssemblyHandle = CreateFileA(AssemblyFilename,
-                                        GENERIC_READ,
-                                        FILE_SHARE_READ,
-                                        0,
-                                        OPEN_EXISTING,
-                                        0,
-                                        0);
+    // char filename[] = "listing_0037_single_register_mov";
+    char filename[] = "listing_0038_many_register_mov";
 
-    if(AssemblyHandle == INVALID_HANDLE_VALUE)
+    FILE *file;
+    errno_t error;
+    size_t bytesRead;
+
+    uint8 instructionBuffer[2] = {};
+    uint8 *bufferPtr = instructionBuffer;
+    uint32 instructionCounter = 0;
+
+    error = fopen_s(&file, filename, "rb");
+    if(!file)
     {
-        printf("Invalid file handle");
-        return 1;
+        printf("Unable to open '%s'\n", filename);
+        exit(EXIT_FAILURE);
     }
-    printf("opened successfully\n");
 
-    // read file
+    printf("; %s:\n", filename);
+    printf("bits 16\n");
 
-    // parse line
-    // produce disassembly
+    // read file in 16 bit chunks
+    // TODO (Aaron): Eventually, the length read will have to depend on the instruction
+    bytesRead = fread(bufferPtr, 1, 2, file);
+    instructionCounter++;
+    while(bytesRead)
+    {
+        Assert(bytesRead == 2);
 
-    printf("closing %s\n", AssemblyFilename);
-    CloseHandle(AssemblyHandle);
+        // parse instructions & produce disassembly
+        decode_instruction(instructionBuffer[0], instructionBuffer[1]);
+
+        // read next instructions
+        bytesRead = fread(bufferPtr, 1, 2, file);
+        instructionCounter++;
+    }
+
+    fclose(file);
 
     return 0;
 }
