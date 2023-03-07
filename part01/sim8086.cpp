feb0f2bff29dfcf81a1eec0b91ada8ff8c37d653
Aaron <pinatamostgrim@proton.me>
2023-03-07 12:15:35 -0400

    Create struct for working with instructions

--- part01/sim8086.cpp
+++ part01/sim8086.cpp
@@ -40,6 +40,25 @@ global_variable const char *RegisterEncodingsW1[]
     "di",
 };
 
+typedef struct instruction_data
+{
+    union
+    {
+        uint8 buffer[6] = {};
+        struct
+        {
+            uint8 byte0;
+            uint8 byte1;
+            uint8 byte2;
+            uint8 byte3;
+            uint8 byte4;
+            uint8 byte5;
+        };
+    };
+    uint8 *bufferPtr = buffer;
+    uint32 instructionCount = 0;
+} instruction_buffer;
+
 int main(int argc, char const *argv[])
 {
     if (argc > 2)
@@ -58,9 +77,7 @@ int main(int argc, char const *argv[])
     errno_t error;
     size_t bytesRead;
 
-    uint8 instructionBuffer[6] = {};
-    uint8 *bufferPtr = instructionBuffer;
-    uint32 instructionCounter = 0;
+    instruction_buffer instructions = {};
 
     error = fopen_s(&file, filename, "rb");
     if(!file)
@@ -73,39 +90,35 @@ int main(int argc, char const *argv[])
     printf("bits 16\n");
 
     // read initial instruction byte for parsing
-    bytesRead = fread(bufferPtr, 1, 1, file);
-    instructionCounter++;
+    bytesRead = fread(instructions.bufferPtr, 1, 1, file);
+    instructions.instructionCount++;
 
     // main loop
     while(bytesRead)
     {
         // parse instruction & produce disassembly
-        uint8 byte0 = instructionBuffer[0];
-
         const char *instructionStr = "?";
         const char *destStr = "?";
         const char *sourceStr = "?";
 
         // mov - register/memory to/from register (0b100010)
-        if ((byte0 >> 2) == 34)
+        if ((instructions.byte0 >> 2) == 34)
         {
             instructionStr = "mov";
 
             // parse initial instruction byte
-            uint8 direction = (byte0 >> 1) & (0b00000001);
-            uint8 width = byte0 & 0b00000001;
+            uint8 direction = (instructions.byte0 >> 1) & (0b00000001);
+            uint8 width = instructions.byte0 & 0b00000001;
 
             // read second instruction byte and parse it
-            bufferPtr = instructionBuffer + 1;
-            fread(bufferPtr, 1, 1, file);
-            uint8 byte1 = instructionBuffer[1];
+            instructions.bufferPtr = instructions.buffer + 1;
+            fread(instructions.bufferPtr, 1, 1, file);
 
-            uint8 mod = (byte1 >> 6);
-            uint8 reg = (byte1 >> 3) & (0b00000111);
-            uint8 rm = byte1 & 0b00000111;
+            uint8 mod = (instructions.byte1 >> 6);
+            uint8 reg = (instructions.byte1 >> 3) & (0b00000111);
+            uint8 rm = instructions.byte1 & 0b00000111;
 
-            // determine how many bits of displacement to read and read it
-
+            // determine how many bits of displacement required and read it
             // memory mode, no displacement
             if(mod == 0b0)
             {
@@ -151,9 +164,9 @@ int main(int argc, char const *argv[])
         printf("%s %s,%s\n", instructionStr, destStr, sourceStr);
 
         // read next instructions
-        bufferPtr = instructionBuffer;
-        bytesRead = fread(bufferPtr, 1, 1, file);
-        instructionCounter++;
+        instructions.bufferPtr = instructions.buffer;
+        bytesRead = fread(instructions.bufferPtr, 1, 1, file);
+        instructions.instructionCount++;
     }
 
     fclose(file);
