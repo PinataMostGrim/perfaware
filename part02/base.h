a77dfae6b108c2ebb164157ebfaba73f3ae5d534
Aaron <pinatamostgrim@proton.me>
2023-06-02 19:37:49 -0300

    squash with 'Import 'base.h' from grimlib'

--- part02/base.h
+++ part02/base.h
@@ -45,10 +45,10 @@ typedef uint16_t U16;
 typedef uint32_t U32;
 typedef uint64_t U64;
 
-typedef uint8_t  S8;
-typedef uint16_t S16;
-typedef uint32_t S32;
-typedef uint64_t S64;
+typedef int8_t  S8;
+typedef int16_t S16;
+typedef int32_t S32;
+typedef int64_t S64;
 
 typedef S8  B8;
 typedef S16 B16;
