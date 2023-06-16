3fbc074490234f411b37f8f7c500a14c8120d5d6
Aaron <pinatamostgrim@proton.me>
2023-06-16 20:39:19 -0300

    Add 'PushSizeZero_()' to 'memory_arena'
    PushSizeZero() is used to zero out the memory being allocated by the call.

--- common/src/memory_arena.c
+++ common/src/memory_arena.c
@@ -1,7 +1,6 @@
 /* TODO (Aaron):
-    - Add string literals to assert expressions to give context
-    - Add PushSizeZero_ to ensure memory is zeroed out
 */
+
 #include "base.h"
 #include "memory_arena.h"
 
@@ -17,7 +16,21 @@ global_function void InitializeArena(memory_arena *arena, memory_index size, U8 
 
 global_function void *PushSize_(memory_arena *arena, memory_index size)
 {
-    Assert((arena->Used + size) <= arena->Size);
+    Assert(((arena->Used + size) <= arena->Size) && "Attempted to allocate more space than the arena has remaining");
+
+    void *result = arena->BasePtr + arena->Used;
+    arena->Used += size;
+    arena->PositionPtr = arena->BasePtr + arena->Used;
+
+    return result;
+}
+
+
+global_function void *PushSizeZero_(memory_arena *arena, memory_index size)
+{
+    Assert((arena->Used + size) <= arena->Size && "Attempted to allocate more space than the arena has remaining");
+
+    MemorySet(arena->PositionPtr, 0, size);
 
     void *result = arena->BasePtr + arena->Used;
     arena->Used += size;
@@ -29,7 +42,7 @@ global_function void *PushSize_(memory_arena *arena, memory_index size)
 
 global_function void *PopSize_(memory_arena *arena, memory_index size)
 {
-    Assert(arena->Used >= size);
+    Assert(arena->Used >= size && "Attempted to free more space than has been filled");
 
     arena->Used -= size;
     arena->PositionPtr = arena->BasePtr + arena->Used;
