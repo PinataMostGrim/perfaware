dee2c76b2c0347db9dd22912196f603472f9106e
Aaron <pinatamostgrim@proton.me>
2023-06-16 15:13:34 -0300

    Add missing include guards to headers

--- sim8086/src/sim8086_gui.h
+++ sim8086/src/sim8086_gui.h
@@ -1,3 +1,6 @@
+#ifndef SIM8086_GUI_H
+#define SIM8086_GUI_H
+
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_win32.h"
@@ -5,3 +8,5 @@
 #include <stdbool.h>
 
 static void DrawGui(ImGuiIO *io, bool *show_demo_window, bool *show_another_window, ImVec4 *clear_color);
+
+#endif // SIM8086_GUI_H
--- sim8086/src/sim8086_platform.h
+++ sim8086/src/sim8086_platform.h
@@ -1,3 +1,6 @@
+#ifndef SIM8086_PLATFORM_H
+#define SIM8086_PLATFORM_H
+
 #include "base.h"
 #include "memory_arena.h"
 
@@ -9,3 +12,5 @@ typedef struct
     memory_arena Arena;
     B32 IsInitialized;
 } sim8086_memory;
+
+#endif // SIM8086_PLATFORM_H
