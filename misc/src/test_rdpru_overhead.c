7197c633b14ffbe520cea41ddfeb5dab95cf4783
Aaron <pinatamostgrim@proton.me>
2024-11-02 14:36:43 -0300

    Add script for experimenting with rdpru

---
+++ misc/src/test_rdpru_overhead.c
@@ -0,0 +1,142 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#define REPETITION_TESTER_IMPLEMENTATION
+#include "../../common/src/repetition_tester.h"
+
+#define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
+#define UNUSED(x) (void)(x)
+
+typedef uint8_t u8;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int32_t b32;
+
+typedef u64 function_ptr(u64 count);
+
+static u64 NoPMCs(u64 count);
+static u64 Rdtsc(u64 count);
+static u64 MPerf(u64 count);
+static u64 APerf(u64 count);
+
+typedef struct test_function test_function;
+struct test_function
+{
+    char const *Name;
+    function_ptr *Func;
+};
+
+
+test_function TestFunctions[] =
+{
+    {"NoPMCs", NoPMCs},
+    {"Rdtsc", Rdtsc},
+    {"MPerf", MPerf},
+    {"APerf", APerf},
+};
+
+
+static uint64_t read_mperf()
+{
+    u32 low, high;
+    __asm__ volatile("mov $0, %%ecx; rdpru"
+                 : "=a" (low), "=d" (high)
+                 :
+                 : "ecx");
+
+    return ((u64)high << 32) | low;
+}
+
+static uint64_t read_aperf()
+{
+    u32 low, high;
+    __asm__ volatile("mov $1, %%ecx; rdpru"
+                 : "=a" (low), "=d" (high)
+                 :
+                 : "ecx");
+
+    return ((u64)high << 32) | low;
+}
+
+
+static u64 NoPMCs(u64 count)
+{
+    u64 sum = 0;
+    for (int i = 0; i < count; ++i)
+    {
+        sum += 1;
+    }
+
+    return sum;
+}
+
+
+static u64 Rdtsc(u64 count)
+{
+    u64 sum = 0;
+    for (int i = 0; i < count; ++i)
+    {
+        u64 start = ReadCPUTimer();
+        sum += 1;
+        u64 end = ReadCPUTimer();
+    }
+
+    return sum;
+}
+
+
+static u64 MPerf(u64 count)
+{
+    u64 sum = 0;
+    for (int i = 0; i < count; ++i)
+    {
+        u64 start = read_mperf();
+        sum += 1;
+        u64 end = read_mperf();
+    }
+
+    return sum;
+}
+
+
+static u64 APerf(u64 count)
+{
+    u64 sum = 0;
+    for (int i = 0; i < count; ++i)
+    {
+        u64 start = read_aperf();
+        sum += 1;
+        u64 end = read_aperf();
+    }
+
+    return sum;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    InitializeTester();
+    repetition_tester testers[ArrayCount(TestFunctions)] = {0};
+    u64 count = 10000;
+
+    for(u32 testFunctionIndex = 0; testFunctionIndex < ArrayCount(TestFunctions); testFunctionIndex++)
+    {
+        test_function testFunction = TestFunctions[testFunctionIndex];
+        repetition_tester tester = testers[testFunctionIndex];
+
+        NewTestWave(&tester, 0, TesterGlobals.CPUTimerFrequency, TesterGlobals.SecondsToTry);
+        fprintf(stdout, "\n--- %s %lu ---\n", testFunction.Name, count);
+
+        while(IsTesting(&tester))
+        {
+            BeginTime(&tester);
+            testFunction.Func(count);
+            EndTime(&tester);
+        }
+    }
+
+    fprintf(stdout, "\n");
+
+    return 0;
+}
