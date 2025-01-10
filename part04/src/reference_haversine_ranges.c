cc9eb0ce1dedfb153b06589ae13023b0e618f71b
Aaron <pinatamostgrim@proton.me>
2025-01-10 14:22:29 -0400

    Cleaner implementation of 'reference_haversine_ranges' based on Casey's example

--- part04/src/reference_haversine_ranges.c
+++ part04/src/reference_haversine_ranges.c
@@ -1,4 +1,6 @@
+#include <float.h>
 #include <math.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <sys/stat.h>
 
@@ -22,34 +24,70 @@
 #include "reference_haversine_parser.c"
 
 
+typedef struct range range;
+struct range
+{
+    F64 Min;
+    F64 Max;
+};
+
 typedef struct haversine_ranges haversine_ranges;
 struct haversine_ranges
 {
-    V2F64 sin_input_range;
-    V2F64 sin_output_range;
-
-    V2F64 cos_input_range;
-    V2F64 cos_output_range;
-
-    V2F64 square_input_range;
-    V2F64 square_output_range;
+    range Sin;
+    range Cos;
+    range ArcSin;
+    range Sqrt;
 };
 
 
-global_function void UpdateRanges(V2F64 *range, F64 value)
+global_variable haversine_ranges Ranges = {0};
+
+
+global_function void RangeInclude(range *range, F64 value)
 {
-    if (value < range->x)
-    {
-        range->x = value;
-    }
-    else if (value > range->y)
-    {
-        range->y = value;
-    }
+    if (value < range->Min) { range->Min = value; }
+    if (value > range->Max) { range->Max = value; }
 }
 
 
-global_function F64 CalculateHaversineWithRanges(F64 X0, F64 Y0, F64 X1, F64 Y1, F64 EarthRadius, haversine_ranges *ranges)
+global_function F64 SinRange(F64 value)
+{
+    RangeInclude(&Ranges.Sin, value);
+    F64 result = sin(value);
+
+    return result;
+}
+
+
+global_function F64 CosRange(F64 value)
+{
+    RangeInclude(&Ranges.Cos, value);
+    F64 result = cos(value);
+
+    return result;
+}
+
+
+global_function F64 ArcSinRange(F64 value)
+{
+    RangeInclude(&Ranges.ArcSin, value);
+    F64 result = asin(value);
+
+    return result;
+}
+
+
+global_function F64 SqrtRange(F64 value)
+{
+    RangeInclude(&Ranges.Sqrt, value);
+    F64 result = sqrt(value);
+
+    return result;
+}
+
+
+global_function F64 ReferenceHaversineRanges(F64 X0, F64 Y0, F64 X1, F64 Y1, F64 EarthRadius)
 {
     F64 lat1 = Y0;
     F64 lat2 = Y1;
@@ -61,70 +99,12 @@ global_function F64 CalculateHaversineWithRanges(F64 X0, F64 Y0, F64 X1, F64 Y1,
     lat1 = RadiansFromDegrees(lat1);
     lat2 = RadiansFromDegrees(lat2);
 
-    F64 dLatSinInput = dLat/2.0;
-    UpdateRanges(&ranges->sin_input_range, dLatSinInput);
-
-    F64 dLatSin = sin(dLatSinInput);
-    UpdateRanges(&ranges->sin_output_range, dLatSin);
-
-    F64 dLonSinInput = dLon/2;
-    UpdateRanges(&ranges->sin_input_range, dLonSinInput);
-
-    F64 dLonSin = sin(dLonSinInput);
-    UpdateRanges(&ranges->sin_output_range, dLonSin);
-
-    F64 lat1Cos = cos(lat1);
-    UpdateRanges(&ranges->cos_input_range, lat1);
-    UpdateRanges(&ranges->cos_output_range, lat1Cos);
-
-    F64 lat2Cos = cos(lat2);
-    UpdateRanges(&ranges->cos_input_range, lat2);
-    UpdateRanges(&ranges->cos_output_range, lat2Cos);
-
-    F64 dLatSinSquare = Square(dLatSin);
-    F64 dLonSinSquare = Square(dLonSin);
-
-    F64 a = dLatSinSquare + lat1Cos * lat2Cos * dLonSinSquare;
-
-    // TODO (Aaron): Add ranges for asin and sqrt
-    F64 c = 2.0*asin(sqrt(a));
+    F64 a = Square(SinRange(dLat/2.0)) + CosRange(lat1)*CosRange(lat2)*Square(SinRange(dLon/2));
+    F64 c = 2.0*ArcSinRange(SqrtRange(a));
 
     F64 Result = EarthRadius * c;
 
     return Result;
-}
-
-
-global_function F64 ReferenceSumHaversineRanges(haversine_setup setup, haversine_ranges *ranges)
-{
-    U64 pairCount = setup.PairCount;
-    haversine_pair *pairs = setup.Pairs;
-
-    F64 sum = 0;
-
-    F64 sumCoeficient = 1 / (F64)pairCount;
-    for (U64 pairIndex = 0; pairIndex < pairCount; ++pairIndex)
-    {
-        haversine_pair pair = pairs[pairIndex];
-        F64 earthRadius = EARTH_RADIUS;
-        F64 dist = CalculateHaversineWithRanges(pair.point0.x, pair.point0.y, pair.point1.x, pair.point1.y, earthRadius, ranges);
-        sum += sumCoeficient * dist;
-    }
-
-    return sum;
-}
-
-
-global_function void PrintHaversineRanges(haversine_ranges ranges)
-{
-    fprintf(stdout, "Ranges:\n");
-
-    fprintf(stdout, "Sine input, min: %f, max: %f\n", ranges.sin_input_range.x, ranges.sin_input_range.y);
-    fprintf(stdout, "Sine output, min: %f, max: %f\n", ranges.sin_output_range.x, ranges.sin_output_range.y);
-
-    fprintf(stdout, "Cos input, min: %f, max: %f\n", ranges.cos_input_range.x, ranges.cos_input_range.y);
-    fprintf(stdout, "Cos output, min: %f, max: %f\n", ranges.cos_output_range.x, ranges.cos_output_range.y);
-    fprintf(stdout, "\n");
 }
 
 
@@ -140,26 +120,32 @@ int main(int argCount, char const *args[])
     char *answersFilename = (char *)args[2];
 
     haversine_setup setup = SetupHaversine(haversinePairsFilename, answersFilename);
-    haversine_ranges ranges = {0};
-
     if (SetupIsValid(setup))
     {
-        F64 referenceSum = setup.SumAnswer;
+        range invertedInfinite = {DBL_MAX, -DBL_MAX};
+        Ranges.Sin = invertedInfinite;
+        Ranges.Cos = invertedInfinite;
+        Ranges.ArcSin = invertedInfinite;
+        Ranges.Sqrt = invertedInfinite;
 
-        U64 individualErrorCount = ReferenceVerifyHaversine(setup);
-        F64 computedSum = ReferenceSumHaversineRanges(setup, &ranges);
-
-        U64 sumErrorCount = 0;
-        sumErrorCount += !ApproxAreEqual(computedSum, referenceSum);
-
-        if (sumErrorCount || individualErrorCount)
+        for (U64 pairIndex = 0; pairIndex < setup.PairCount; ++pairIndex)
         {
-            fprintf(stderr, "[WARNING]: %lu haversines mismatched, %lu sum mismatches\n",
-                    individualErrorCount, sumErrorCount);
+            haversine_pair pair = setup.Pairs[pairIndex];
+
+            F64 distanceSum = ReferenceHaversineRanges(pair.point0.x, pair.point0.y, pair.point1.x, pair.point1.y, EARTH_RADIUS);
+            F64 referenceSum = ReferenceHaversine(pair.point0.x, pair.point0.y, pair.point1.x, pair.point1.y, EARTH_RADIUS);
+            if (distanceSum != referenceSum)
+            {
+                fprintf(stderr, "[ERROR] Range-check version doesn't match reference version.\n");
+            }
         }
 
         fprintf(stdout, "\n");
-        PrintHaversineRanges(ranges);
+        fprintf(stdout, "Ranges:\n");
+        fprintf(stdout, "Sin: %f, %f\n", Ranges.Sin.Min, Ranges.Sin.Max);
+        fprintf(stdout, "Cos: %f, %f\n", Ranges.Cos.Min, Ranges.Cos.Max);
+        fprintf(stdout, "ArcSin: %f, %f\n", Ranges.ArcSin.Min, Ranges.ArcSin.Max);
+        fprintf(stdout, "Sqrt: %f, %f\n", Ranges.Sqrt.Min, Ranges.Sqrt.Max);
         fprintf(stdout, "\n");
     }
     else
