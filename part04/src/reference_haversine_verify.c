c9987ab6cf65bcae801a041999e7044998931e53
Aaron <pinatamostgrim@proton.me>
2025-07-26 23:34:09 -0300

    Remove several custom sin variants and repetition tests

--- part04/src/reference_haversine_verify.c
+++ part04/src/reference_haversine_verify.c
@@ -7,9 +7,6 @@
 #include "base_inc.h"
 #include "base_types.c"
 
-#define REPETITION_TESTER_IMPLEMENTATION
-#include "repetition_tester.h"
-
 #if _MSC_VER
 #include <intrin.h>
 #else
@@ -55,15 +52,6 @@ struct function_error
 };
 
 
-typedef struct named_math_function named_math_function;
-struct named_math_function
-{
-    char const *Name;
-    math_func *Func;
-
-};
-
-
 static F64 Square(F64 x)
 {
     F64 result = x*x;
@@ -81,34 +69,8 @@ static F64 CustomSin(F64 x)
     return result;
 }
 
+
 static F64 CustomSinRR(F64 x)
-{
-    // Range reduction
-
-    // If input is 0 or above, do nothing
-    // If input is below 0, invert it so that it is above zero, and then invert the answer before returning later.
-    B32 flipped = 0;
-    if (x < 0)
-    {
-        x = x * -1;
-        flipped = 1;
-    }
-
-    F64 x2 = Square(x);
-    F64 a = -4.0 / Square(Pi64);
-    F64 b = 4.0 / Pi64;
-
-    F64 result = (a * x2) + (b * x);
-
-    if (flipped)
-    {
-        result = result * -1;
-    }
-
-    return result;
-}
-
-static F64 CustomSinRRMultiplier(F64 x)
 {
     // Range reduction
 
@@ -123,82 +85,6 @@ static F64 CustomSinRRMultiplier(F64 x)
 
     F64 result = (a * x2) + (b * x);
     result *= multiplier;
-
-    return result;
-}
-
-int GetSignMultiplierNaive(double x)
-{
-    int result = 0;
-
-    if (x >= 0)
-    {
-        result = 1;
-    }
-    else
-    {
-        result = -1;
-    }
-
-    return result;
-}
-
-
-static F64 CustomSinRRSMN(F64 x)
-{
-    // Range reduction, sign multiplier naive
-
-    // If input is 0 or above, do nothing
-    // If input is below 0, invert it so that it is above zero, and then invert the answer before returning later.
-    double signChange = GetSignMultiplierNaive(x);
-    x = x * signChange;
-
-    F64 x2 = Square(x);
-    F64 a = -4.0 / Square(Pi64);
-    F64 b = 4.0 / Pi64;
-
-    F64 result = (a * x2) + (b * x);
-
-    // Set the sign back
-    result = result * signChange;
-
-    return result;
-}
-
-
-int GetSignMultiplierBitShift(double x)
-{
-    union
-    {
-        double d;
-        uint64_t u;
-    } converter = { .d = x };
-
-    uint64_t sign_bit = converter.u >> 63;
-
-    uint64_t is_non_zero = (converter.u & 0x7FFFFFFFFFFFFFFF) != 0;
-    int result = (int)(1 - (2 * (sign_bit & is_non_zero)));
-    return result;
-}
-
-
-static F64 CustomSinRRSMBS(F64 x)
-{
-    // Range reduction, sign multiplier naive
-
-    // If input is 0 or above, multiply by 1
-    // If input is below 0, invert its sign so that it is above zero, and then invert the answer before returning later.
-    double signChange = GetSignMultiplierNaive(x);
-    x = x * signChange;
-
-    F64 x2 = Square(x);
-    F64 a = -4.0 / Square(Pi64);
-    F64 b = 4.0 / Pi64;
-
-    F64 result = (a * x2) + (b * x);
-
-    // Set the sign back
-    result = result * signChange;
 
     return result;
 }
@@ -291,33 +177,6 @@ static void PrintError(function_error error)
 }
 
 
-static double SimpleAdvanceInput(double x)
-{
-    x = x + 0.1;
-    if (x > Pi64)
-    {
-        x -= Pi64;
-    }
-
-    return x;
-}
-
-static double AdvanceInput(double x)
-{
-    x += Pi64;
-    x = x + 0.1;
-
-    if (x > (Pi64 * 2))
-    {
-        x -= (Pi64 * 2);
-    }
-
-    x -= Pi64;
-
-    return x;
-}
-
-
 int main(int argc, char const *argv[])
 {
     // Note (Aaron): Enable to verify reference values
@@ -328,11 +187,10 @@ int main(int argc, char const *argv[])
     CheckHardcodedAnswer("sqrt", sqrt, Reference_Sqrt, ArrayCount(Reference_Sqrt));
 #endif
 
-#if 1
     U32 stepCount = 100000000;
 
     printf("Calulating maximum function error:\n");
-    // PrintErrorHeader();
+
     function_error error = MeasureMaximumFunctionError(CustomSin, sin, 0, Pi64, stepCount, "CustomSin: 0 to Pi");
     PrintError(error);
 
@@ -340,12 +198,6 @@ int main(int argc, char const *argv[])
     PrintError(error);
 
     error = MeasureMaximumFunctionError(CustomSinRR, sin, -Pi64, Pi64, stepCount, "CustomSinRR: -Pi to Pi");
-    PrintError(error);
-
-    error = MeasureMaximumFunctionError(CustomSinRRSMN, sin, -Pi64, Pi64, stepCount, "CustomSinRRSMN: -Pi to Pi");
-    PrintError(error);
-
-    error = MeasureMaximumFunctionError(CustomSinRRSMBS, sin, -Pi64, Pi64, stepCount, "CustomSinRRSMBS: -Pi to Pi");
     PrintError(error);
 
     function_error errorCos = MeasureMaximumFunctionError(CustomCos, cos, -Pi64/2, Pi64/2, stepCount, "CustomCos");
@@ -356,52 +208,6 @@ int main(int argc, char const *argv[])
 
     function_error errorSqrt = MeasureMaximumFunctionError(CustomSqrt, sqrt, 0, 1, stepCount, "CustomSqrt");
     PrintError(errorSqrt);
-#endif
-
-#if 0
-    double input = -1.5707963110869332;
-    int smn = GetSignMultiplierNaive(input);
-    int smbs = GetSignMultiplierBitShift(input);
-
-    int bp = 0;
-#endif
-
-#if 0
-    double input = 0;
-    InitializeTesterGlobals();
-
-    named_math_function testFunctions[] =
-    {
-        {"sin", sin},
-        {"CustomSin", CustomSin },
-        {"CustomSinRR", CustomSinRR },
-        {"CustomSinRRMultiplier", CustomSinRRMultiplier },
-        {"CustomSinRRSMN", CustomSinRRSMN },
-        {"CustomSinRRSMBS", CustomSinRRSMBS },
-    };
-
-    printf("\n");
-
-    for (int functionIndex = 0; functionIndex < ArrayCount(testFunctions); ++functionIndex)
-    {
-        named_math_function function = testFunctions[functionIndex];
-        printf("%s:\n", function.Name);
-
-        repetition_tester tester = {0};
-        NewTestWave(&tester, 0, TesterGlobals.CPUTimerFrequency, 30);
-
-        while(IsTesting(&tester))
-        {
-            input = SimpleAdvanceInput(input);
-
-            BeginTime(&tester);
-            function.Func(input);
-            EndTime(&tester);
-        }
-
-        printf("\n");
-    }
-#endif
 
     return 0;
 }
